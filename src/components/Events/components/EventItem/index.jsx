39962062cb925901be5ec1c8aa3a6500b9833ea6
JuanPabdl <a00825316@itesm.mx>
2024-02-14 16:29:46 -0600

    like & unlike events

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,19 +1,28 @@
 //import { Link } from 'react-router-dom';
 import styles from './EventItem.module.css'
+import HeartFilled from '../../../../assets/hearth-filled.png';
+import HeartUnfilled from'../../../../assets/hearth-unfilled.png';
+import useLikeEvents from '../../../../hooks/useLikeEvents';
 
 const EventItem = ({info, id, name, image, onEventClick}) => {
+    const {isEventLiked, toggleEventLike} = useLikeEvents(id);
 
     const handleSeeMoreClick = (event) =>{
         event.stopPropagation()
         onEventClick(id)
     };
 
+    const handleHeartClick = () => {
+        toggleEventLike();
+    }
+
     return (
         <div onClick={()=> console.log('Padre clickeado')} className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
-            <img src={image} 
-            alt={name} 
-            width={200}
-             height={200}/>
+            <div className={styles.imageContainer}>
+                <img src={ isEventLiked ? HeartFilled : HeartUnfilled } alt='Heart button' className={styles.heartImage} onClick={handleHeartClick}/>
+                <img src={image} alt={name} width={200} height={200}/>
+            </div>
+            
             <div className={styles.eventInfoContainer}>
                 <h4 className={styles.eventName}>{name}</h4>
                 <p className={styles.eventInfo}>{info}</p>
